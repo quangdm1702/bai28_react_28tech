95b2b5cbcd03dcf6e650ea86a64cae538d472998
quangdm1702 <doanminhquang17022003@gmail.com>
2024-08-27 15:56:58 +0700

    Events React

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,7 +1,42 @@
 function NavMenu() {
+
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  const handleFocus = (e) => {
+    e.target.classList.add('input--active');
+    console.log(e.target);
+  }
+
+  const handleBlur = (e) => {
+    e.target.classList.remove('input--active');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();// ngan chn sk mac dinh khi nhan submit form
+    console.log(e.target[0].value);
+  }
+
   return (
     <>
-      <div className="box">NavMenu</div>
+      <div className="box">
+        <div>NavMenu</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="input"
+            type="text"
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          ></input>
+          <button onClick={handleClick}>Click Me</button>
+        </form>
+      </div>
     </>
   );
 }
