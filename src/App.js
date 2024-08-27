c64946357961bf808606be6119b39643204093e4
quangdm1702 <doanminhquang17022003@gmail.com>
2024-08-27 14:16:06 +0700

    JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = 'Doan Minh Quang';
+  const css = {
+    color: 'white',
+    backgroundColor: 'black'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="test" style={css}>
+        Xin chao {name}
+      </div>
+
+      <div className="test" style={css}>
+        Xin chao {name}
+      </div>
+    </>
   );
 }
 
